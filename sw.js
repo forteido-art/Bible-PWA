9ef613e5964ac2da102b32af6eb41f439d13c0b2
forteido-art <forteido@gmail.com>
2026-07-13 08:52:26 +0100

    sw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bible-v4.3';
+const CACHE = 'bible-v4.4';
 const ASSETS = [
   '/', 'index.html', 'style.css', 'app.js', 
   'bible.js', 'manifest.json', 'icon-192.png', 'icon-512.png'
