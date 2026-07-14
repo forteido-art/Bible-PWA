4f2e990f93dd01254b569f007682274eaac5ebaa
forteido-art <forteido@gmail.com>
2026-07-14 03:24:22 +0100

    Finished

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tkh-bible-v2.3';
+const CACHE_NAME = 'tkh-bible-v3';
 const FILES = ["/", "/index.html", "/kjv.json", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 const ASSETS = [
   './',
